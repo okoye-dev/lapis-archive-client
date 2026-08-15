c45bf5d892b8156748724efca5fb2bf86522d8e6
okoye-dev <okoyedivine145@gmail.com>
2026-08-15 19:51:11 +0100

    update transitions

--- components/ui/toast.tsx
+++ components/ui/toast.tsx
@@ -25,7 +25,7 @@ const ToastViewport = React.forwardRef<
 ToastViewport.displayName = ToastPrimitives.Viewport.displayName;
 
 const toastVariants = cva(
-  "group pointer-events-auto relative flex w-full max-w-[20rem] items-center justify-between space-x-3 overflow-hidden rounded-md border p-3 pr-9 shadow-lg transition-all sm:max-w-none sm:space-x-4 sm:p-6 sm:pr-8 data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=closed]:fade-out-80 data-[state=closed]:slide-out-to-right-full data-[state=open]:slide-in-from-top-full data-[state=open]:sm:slide-in-from-bottom-full",
+  "group pointer-events-auto relative flex w-full max-w-[20rem] items-center justify-between space-x-3 overflow-hidden rounded-md border p-3 pr-9 shadow-lg transition-all sm:max-w-none sm:space-x-4 sm:p-6 sm:pr-8 data-[swipe=cancel]:translate-x-0 data-[swipe=end]:translate-x-[var(--radix-toast-swipe-end-x)] data-[swipe=move]:translate-x-[var(--radix-toast-swipe-move-x)] data-[swipe=move]:transition-none duration-300 ease-out data-[state=open]:animate-in data-[state=closed]:animate-out data-[swipe=end]:animate-out data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95 data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95",
   {
     variants: {
       variant: {
